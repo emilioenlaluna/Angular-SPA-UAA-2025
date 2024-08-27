f4e7b57d25a334f413602d61a028b9ba9fef0a19
Emilio Luna <al244182@edu.uaa.mx>
2024-08-27 07:51:25 -0600

    Binaries and Notes
    Delete Swagger
    
    Create dotnet.yml
    
    Delete Binaries And OBJ
    
    First Entity
    
    DB Context
    
    EF Migrations And User Controller
    
    Completed Client And API
    
    Update 20 Sept
    
    DB Changes
    
    Auth Services Client
    
    Nav Comp IF Validations
    
    Semana 30 Sep - 4 Oct
    
    Create README.md
    
    Squashed commit of the following:
    
    commit 8eaf6e6f027ed2429450adaae4ac8e04239ac995
    Merge: 2ec97ff e3183de
    Author: Emilio Luna <74982686+emilioenlaluna@users.noreply.github.com>
    Date:   Mon Oct 7 13:21:35 2024 +0000
    
        Merge pull request #4 from emilioenlaluna/Parcial-II
    
        Merge pull request #3 from emilioenlaluna/emilioenlaluna-patch-1
    
    commit 2ec97ff879832bbfc478957be0802a5cbe796dd1
    Merge: 801ed5d 2534356
    Author: Emilio Luna <74982686+emilioenlaluna@users.noreply.github.com>
    Date:   Mon Oct 7 03:09:18 2024 +0000
    
        Merge pull request #2 from emilioenlaluna/emilioenlaluna-patch-1
    
        Create README.md
    
    Segundo Parcial #1
    
    Parcial II  Routing Components
    
    Parcial II Nav Componets
    
    Parcial II Add Routing
    
    Parcial II Notifiactions and toast services and dependecies
    
    Parcial II Add bootstrap watcher and Guards
    
    Parcial II Code refactoring using new config and buggy controller
    
    Parcial II Exception Middleware and interceptors in SPA
    
    Parcial II Server error handling and client components
    
    Parcial II New Models and relations
    
    Parcial II Seeds
    
    Parcial II Repository Patern
    
    Parcial II AutoMappers and Users Controllers
    
    Parcial II Automapper config and queyble entensions

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,37 @@
+namespace API.Controllers;
+using API.Data;
+using API.DTOs;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+[Authorize]
+public class UsersController : BaseApiController
+{
+    private readonly IUserRepository _repository;
+
+    public UsersController(IUserRepository repository)
+    {
+        _repository = repository;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<MemberResponse>>> GetAllAsync()
+    {
+        var members = await _repository.GetMembersAsync();
+        return Ok(members);
+    }
+
+    [HttpGet("{username}")] // api/users/Calamardo
+    public async Task<ActionResult<MemberResponse>> GetByUsernameAsync(string username)
+    {
+        var member = await _repository.GetMemberAsync(username);
+
+        if (member == null)
+        {
+            return NotFound();
+        }
+
+        return member;
+    }
+}
