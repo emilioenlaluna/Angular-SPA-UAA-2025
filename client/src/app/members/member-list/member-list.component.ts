3ca802087b3facecc91fe290fce7c411db3a5e26
Emilio Luna <al244182@edu.uaa.mx>
2025-02-28 21:23:18 -0600

    Paginacion Cliente, angular seriico y componente

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,25 +1,34 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { MembersService } from '../../_services/members.service';
-import { Member } from '../../_models/member';
 import { MemberCardComponent } from '../member-card/member-card.component';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
 
 @Component({
   selector: 'app-member-list',
   standalone: true,
-  imports: [MemberCardComponent],
+  imports: [MemberCardComponent, PaginationModule],
   templateUrl: './member-list.component.html',
   styleUrl: './member-list.component.css'
 })
 export class MemberListComponent implements OnInit {
   membersService = inject(MembersService);
+  pageNumber = 1;
+  pageSize = 5;
   
   ngOnInit(): void {
-    if (this.membersService.members().length === 0) {
+    if (!this.membersService.paginatedResult()) {
       this.loadMembers();
     }
   }
 
   loadMembers() {
-    this.membersService.getMembers();
+    this.membersService.getMembers(this.pageNumber, this.pageSize);
+  }
+
+  pageChanged(event: any) {
+    if (this.pageNumber !== event.page) {
+      this.pageNumber = event.page;
+      this.loadMembers();
+    }
   }
 }
