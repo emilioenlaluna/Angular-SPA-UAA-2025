e8b7929baa9b8db06afcb747d529028580ad1a0c
Emilio Luna <al244182@edu.uaa.mx>
2025-05-08 21:28:26 -0600

    Implementacio del sistema de mensajes en SPA

--- client/src/app/messages/messages.component.ts
+++ client/src/app/messages/messages.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { MessagesService } from '../_services/messages.service';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
+import { FormsModule } from '@angular/forms';
+import { TimeagoModule } from 'ngx-timeago';
+import { Message } from '../_models/message';
+import { RouterModule } from '@angular/router';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
 
 @Component({
   selector: 'app-messages',
   standalone: true,
-  imports: [],
+  imports: [ButtonsModule, FormsModule, TimeagoModule, RouterModule, PaginationModule],
   templateUrl: './messages.component.html',
   styleUrl: './messages.component.css'
 })
-export class MessagesComponent {
+export class MessagesComponent implements OnInit {
+  messagesService = inject(MessagesService);
+  container = "Inbox";
+  pageNumber = 1;
+  pageSize = 10;
+
+  ngOnInit(): void {
+    this.loadMessages();
+  }
+
+  loadMessages() {
+    this.messagesService.getMessages(this.pageNumber, this.pageSize, this.container);
+  }
+
+  getRoute(message: Message) {
+  // if (this.container === "outbox") return `/members/${message.recipientUsername}`;
+  // else return `/members/${message.senderUsername}`;
+    return this.container === "Outbox"
+      ? `/members/${message.recipientUsername}`
+      : `/members/${message.senderUsername}`;
+  }
+
+  pageChanged(event: any) {
+    if (this.pageNumber !== event.page) {
+      this.pageNumber = event.page;
+      this.loadMessages();
+    }
+  }
 
 }
